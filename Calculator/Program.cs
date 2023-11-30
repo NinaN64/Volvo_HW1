769fcc36188c967dbc60c861f61d1f9bf9c8490a
Nina Nowak <259802@student.pwr.edu.pl>
2023-11-30 14:37:12 +0100

    Addition function

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -4,7 +4,41 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Calculator instructions:");
+            Console.WriteLine("Enter two numbers and an operation\nAvailable operations: +, -, *, /, ^, !");
+
+            int First_Number = 0;
+            int Second_Number = 0;
+            string Operation;
+
+            while(true)
+            {
+                //Number Inputs
+                Console.WriteLine("\nFirst number: ");
+                First_Number = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Second number: ");
+                Second_Number = Convert.ToInt32(Console.ReadLine());
+
+                //Operation Input
+                Console.WriteLine("Operation: ");
+                Operation = Console.ReadLine();
+                switch(Operation)
+                {
+                    case "+":
+                        Console.WriteLine("Result: ");
+                        Console.WriteLine(Addition(First_Number, Second_Number));                     
+                        break;
+                    //case "-":
+
+
+                }
+                    
+            }
+        }
+
+        static int Addition(int x, int y)
+        {
+            return x + y;
         }
     }
 }
