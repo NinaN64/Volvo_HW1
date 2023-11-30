57adc0439da705d03306ae74208fef7f3f9122da
Nina Nowak <259802@student.pwr.edu.pl>
2023-11-30 15:35:30 +0100

    Few chaneges to outputs

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -16,8 +16,6 @@
                 //Number Inputs
                 Console.WriteLine("\nFirst number: ");
                 First_Number = Convert.ToDouble(Console.ReadLine());
-                Console.WriteLine("Second number: ");
-                Second_Number = Convert.ToDouble(Console.ReadLine());
 
                 //Operation Input
                 Console.WriteLine("Operation: ");
@@ -25,33 +23,46 @@
                 switch(Operation)
                 {
                     case "!":
-                        Console.WriteLine("Result: ");
-                        Console.WriteLine(Factorial(First_Number));
+                        Console.WriteLine("\n" + First_Number + Operation + " = " + Factorial(First_Number));
                         break;
 
                     case "^":
-                        //Console.WriteLine("Result: ");
-                        Console.WriteLine(Exponentiation(First_Number));
+                        Console.WriteLine("Exponentiation number:");
+                        double ExpNumber = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine("\n" + First_Number + Operation + ExpNumber + " = " + Exponentiation(First_Number, ExpNumber));
                         break;
 
                     case "/":
+                        Console.WriteLine("Second number: ");
+                        Second_Number = Convert.ToDouble(Console.ReadLine());
+
                         Console.WriteLine("Result: ");
-                        Console.WriteLine(Division(First_Number, Second_Number));
+                        Console.WriteLine("\n" + First_Number + Operation + Second_Number + " = " + Division(First_Number, Second_Number));
                         break;
 
                     case "*":
+                        Console.WriteLine("Second number: ");
+                        Second_Number = Convert.ToDouble(Console.ReadLine());
+
                         Console.WriteLine("Result: ");
-                        Console.WriteLine(Multiplication(First_Number, Second_Number));
+                        Console.WriteLine("\n" + First_Number + Operation + Second_Number + " = " + Multiplication(First_Number, Second_Number));
                         break;
 
                     case "-":
+                        Console.WriteLine("Second number: ");
+                        Second_Number = Convert.ToDouble(Console.ReadLine());
+
                         Console.WriteLine("Result: ");
-                        Console.WriteLine(Subtraction(First_Number, Second_Number));
+                        Console.WriteLine("\n" + First_Number + Operation + Second_Number + " = " + Subtraction(First_Number, Second_Number));
                         break;
 
                     case "+":
+                        Console.WriteLine("Second number: ");
+                        Second_Number = Convert.ToDouble(Console.ReadLine());
+
                         Console.WriteLine("Result: ");
-                        Console.WriteLine(Addition(First_Number, Second_Number));                     
+                        Console.WriteLine("\n" + First_Number + Operation + Second_Number + " = " + Addition(First_Number, Second_Number));                     
                         break;                   
                 }
                 Console.WriteLine("Exit? (y/n)");
@@ -71,11 +82,9 @@
             }
             return ResaultNumber;
         }
-        static double Exponentiation(double x)
+        static double Exponentiation(double x, double ExpNumber)
         {
             double ResaultNumber = x;
-            Console.WriteLine("Exponentiation number:");
-            double ExpNumber = Convert.ToInt32(Console.ReadLine());
             for(int  i = 1; i < ExpNumber; i++)
             {
                 ResaultNumber = ResaultNumber * x;
